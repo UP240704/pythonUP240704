81ae4384a848a07e39a04eb559362136c6cb4796
UP240704 <up240704@alumnos.upa.edu.mx>
2025-02-20 11:37:40 -0600

    commit

---
+++ dia2.py
@@ -0,0 +1,25 @@
+# Day 2: 30 Days of python programming 
+firstname = "isai"
+lastname = " Muro González"
+nombre= firstname + lastname
+country="mexico"
+city= "aguascalientes"
+age= 18
+year= 3015
+is_married= False
+is_true= "he likes basketball"
+is_light_on= True
+skills= "basketball and cooking" 
+dislikes= "baseball"
+#excercises level 2
+print (type(firstname))
+print (type(lastname))
+print (type(country))
+print (type(city))
+print (type(age))
+print (type(year))
+print (type(is_married))
+print (type(is_true))
+print (type(is_light_on))
+print (type(skills))
+print (type(dislikes))
