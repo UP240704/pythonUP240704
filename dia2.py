b468e159c77532dfbcaf89ef5fa79c9e00db77c6
UP240704 <up240704@alumnos.upa.edu.mx>
2025-02-25 09:15:24 -0600

    .

--- dia2.py
+++ dia2.py
@@ -12,6 +12,7 @@ is_light_on= True
 skills= "basketball and cooking" 
 dislikes= "baseball"
 #excercises level 2
+#1
 print (type(firstname))
 print (type(lastname))
 print (type(country))
@@ -23,3 +24,40 @@ print (type(is_true))
 print (type(is_light_on))
 print (type(skills))
 print (type(dislikes))
+#2
+print (len(firstname))
+#3
+print (len(lastname)-(len(firstname)))
+#4
+num_one= 5
+num_two= 4
+#5
+total= num_one+num_two
+print(total)
+#6
+diff= num_one-num_two
+print(diff)
+#7
+product= num_two*num_one
+print(product)
+#8
+division= num_one/num_two
+print(division)
+#7
+remainder= num_two%num_one
+#8
+exp= num_one**num_two
+#9
+floor_division= num_one//num_two
+#10
+area_of_circle= 30**2 * 3.1416
+circum_of_circle= 30*2*3.1416
+radius=int(input())
+area_of_circle= radius**2*3.1416
+print (area_of_circle)
+#11
+
+
+
+
+
