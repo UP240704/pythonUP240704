c55d6bbe0040a9cf4a054bbd0a80f7ed176ce83c
UP240704 <up240704@alumnos.upa.edu.mx>
2025-03-04 20:09:25 -0600

    .

--- dia4.py
+++ dia4.py
@@ -5,20 +5,59 @@ tres="of "
 cuatro="python"
 cinco= uno+dos+tres+cuatro
 print(cinco)
+
 ## 2 concatenar
 a="coding"
 b=" for"
 c=" all"
 d= a+b+c
 print(d)
+
 ## 3 
 company="coding for all"
+
 ## 4
 print(company)
+
 ## 5
 print(len(company))
+
 ## 6 
 uppercompany=company.upper()
-## 7h
 
+## 7
+lowercompany=company.lower()
 
+##8
+print(
+company.capitalize(),
+company.title(),
+company.swapcase())
+
+##9
+cc1=company[7:15]
+print(cc1)
+
+##10
+print(company.find("coding"))
+
+##11
+print(company.replace("coding","python"))
+
+##12
+print(company.replace("all","everyone"))
+
+##13
+print(company.split(" "))
+
+##15
+texto="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(texto.split())
+
+##16
+letra1=company[0]
+print(company[0])
+
+##17
+print(company[10])
+##18
