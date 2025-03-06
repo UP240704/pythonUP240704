4b63ecb7d3e3ca66d7845f92c0fd793caf5b6ade
UP240704 <up240704@alumnos.upa.edu.mx>
2025-03-06 09:12:30 -0600

    .

--- dia5.py
+++ dia5.py
@@ -3,4 +3,10 @@ listavacia=list()
 ##2
 lista=list(["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete"])
 print(lista)
+##3
 print(len(lista))
+##4
+listacortada=lista[::3]
+##5
+print(listacortada)
+
