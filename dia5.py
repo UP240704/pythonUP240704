98c6d78906051a9bfc73345d605a34bde7fcbf53
UP240704 <up240704@alumnos.upa.edu.mx>
2025-03-10 21:36:41 -0600

    .

--- dia5.py
+++ dia5.py
@@ -37,63 +37,66 @@ print(empresas_tecnologicas)
 ##11
 empresas_tecnologicas.append('IBM')
 
-##
+##12
 empresas_tecnologicas.insert(3, 'Samsung')
 
-##12
+##13
 empresas_tecnologicas[0] = empresas_tecnologicas[0].upper()
 print(empresas_tecnologicas)
 
-##13
+##14
 print(str.join('#;', empresas_tecnologicas))
 
-##14
+##15
 existe = 'Facebook' in empresas_tecnologicas
 print(existe)
 
-##15
+##16
 empresas_tecnologicas.sort()
 print(empresas_tecnologicas)
 
-##16
+##17
 empresas_tecnologicas.reverse()
 print(empresas_tecnologicas)
 
-##17
+##18
 primeras_tres_empresas = empresas_tecnologicas[0:3]
 print(primeras_tres_empresas)
 
-##18
+##19
 ultimas_empresas = empresas_tecnologicas[6:9]
 print(ultimas_empresas)
 
-##19
+##20
 empresa_unica = empresas_tecnologicas[4:5]
 print(empresa_unica)
 
-##20
-print(empresas_tecnologicas.remove('Samsung'))
-
 ##21
-print(empresas_tecnologicas.remove('IBM'))
+print(empresas_tecnologicas.remove('Facebook'))
 
 ##22
-print(empresas_tecnologicas.remove('Oracle'))
+print(empresas_tecnologicas.remove('IBM'))
 
 ##23
-print(empresas_tecnologicas.remove('Amazon'))
+print(empresas_tecnologicas.remove('Oracle'))
 
 ##24
+print(empresas_tecnologicas.remove[0:9])
+
+##25
+del(empresas_tecnologicas)
+
+##26
 tecnologias_front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 tecnologias_back_end = ['Node', 'Express', 'MongoDB']
 pila_completa_tecnologica = tecnologias_front_end + tecnologias_back_end
 print(pila_completa_tecnologica)
 
-##25
+##27
 copia_pila_completa = pila_completa_tecnologica.copy()
 print(copia_pila_completa)
 
-##26
+##28
 edades = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 edades.sort()
 print(edades)
@@ -105,22 +108,22 @@ edades.append(19)
 edades.append(26)
 print(edades)
 
-##27
+##29
 promedio_edad = sum(edades) / len(edades)
 print(promedio_edad)
 
-##28
+##30
 edades.sort()
 print(abs(edades[0] - promedio_edad))
 print(abs(edades[8] - promedio_edad))
 
-##29
+##31
 from listapaises import countries
 print(countries[int(len(countries)/2)])
 primera_mitad = countries[0:96]
 segunda_mitad = countries[96:192]
 
-##30
+##32
 paises = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
 primeros_tres_paises = paises[0:3] 
 resto_paises = paises[3:7]
