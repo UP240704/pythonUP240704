9743f4174bda6e8f3167a04128c46eeced1497cd
UP240704 <up240704@alumnos.upa.edu.mx>
2025-04-02 10:09:54 -0600

    .

---
+++ Trabajos python/10_Day/dia10.py
@@ -0,0 +1,33 @@
+##1
+count=0
+while count<11:
+    print(count)
+    count=count+1
+
+##2
+ciclo=10
+while ciclo>-1:
+    print(ciclo)
+    ciclo=ciclo-1
+
+##3
+ciclo2=1
+while ciclo2<8:
+    print("#"*ciclo2)
+    ciclo2=ciclo2+1
+
+##4
+fila=8
+columna=8
+for i in range(fila):
+    for j in range(columna):
+        print("#", end=" ")
+    print()
+
+##5
+num1=0
+while num1<11:
+    print(num1, "X", num1, "=", num1*num1)
+    num1=num1+1
+
+##6
