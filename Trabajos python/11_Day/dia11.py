462174f9a0f475a11d46f2275a49cd7ccbed1964
UP240704 <up240704@alumnos.upa.edu.mx>
2025-04-04 09:36:57 -0600

    .

---
+++ Trabajos python/11_Day/dia11.py
@@ -0,0 +1,46 @@
+##1
+def add_two_numbers(numero,numero2):
+    res=numero+numero2
+    return res
+print(add_two_numbers(5,7))
+
+##2
+def area_of_circle(radio):
+    from math import pi
+    area= pi*radio*radio
+    return area
+print(area_of_circle(9))
+
+##3
+def sum_total(*args):
+    total=0
+    for i in args:
+        total=total + i
+    return total
+print (sum_total(1,2,3,4,5))
+    
+##4
+def temperatura(C):
+    F=(C*9/5)+32
+    return F
+print (temperatura(70))
+
+
+##5
+def check_season(mes):
+    if mes in["enero","febrero","diciembre"]:
+        return "invierno"
+    elif mes in["marzo", "abril","mayo"]:
+        return "primavera"
+    elif mes in["junio","julio","agosto"]:
+        return "verano"
+    elif mes in["sepriembre", "octubre","nomviembre"]:
+        return "otoño"
+    else: print("no es un mes")
+print(check_season("enero"))
+
+##6
+def calculate_slope(x1,y1,x2,y2):
+    slope=(y2-y1)/(x2-x1)
+    return slope
+print (calculate_slope(7,8,10,15))
